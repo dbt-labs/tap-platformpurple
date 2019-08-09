36b2a99baad887573ec8ae8a67f2e512487641ef
Connor McArthur <connor@fishtownanalytics.com>
2019-08-09 12:21:40 -0400

    fixes

--- run-from-env-vars.sh
+++ run-from-env-vars.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-### Install target-stitch into a virtualenv
-python3 -m venv target-stitch
-source target-stitch/bin/activate
-pip install target-stitch
-deactivate
-
 ### Load configuration
 echo "$STITCH_CONFIG" > persist.json
 echo "$TAP_CONFIG" > config.json
@@ -14,7 +8,7 @@ echo "$CATALOG" > catalog.json
 aws s3 cp "$TAP_STATE_FILE_S3_PATH" state.json || echo "{}" > state.json
 
 ### Run the tap
-{ tap-platformpurple -s state.json -c config.json --catalog catalog.json | target-stitch/bin/target-stitch -c persist.json > state.log; }
+{ tap-platformpurple -s state.json -c config.json --catalog catalog.json | /app/.target-stitch/bin/target-stitch -c persist.json > state.log; }
 
 tail -n1 state.log > new-state.json
 
