5bfda403760bf275a6fd1d5e78d12c6fde954c17
Connor McArthur <connor@fishtownanalytics.com>
2019-08-09 11:22:58 -0400

    persistence & state storage

--- run-from-env-vars.sh
+++ run-from-env-vars.sh
@@ -5,3 +5,7 @@ echo "$TAP_CONFIG" > config.json
 echo "$CATALOG" > catalog.json
 
 tap-platformpurple -c config.json --catalog catalog.json
+
+{ tap-platformpurple -s state.json -c config.json --catalog catalog.json | target-stitch -c persist.json > state.log; }
+
+tail -n1 state.log > state.json
