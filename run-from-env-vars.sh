09d42732d6d7b78e4b5e8b659d6559650ca0214b
Connor McArthur <connor@fishtownanalytics.com>
2019-08-09 11:43:27 -0400

    persist state

--- run-from-env-vars.sh
+++ run-from-env-vars.sh
@@ -4,8 +4,15 @@ echo "$STITCH_CONFIG" > persist.json
 echo "$TAP_CONFIG" > config.json
 echo "$CATALOG" > catalog.json
 
+aws s3 cp "$TAP_STATE_S3_FILE_PATH" state.json || echo "{}" > state.json
+
 tap-platformpurple -c config.json --catalog catalog.json
 
 { tap-platformpurple -s state.json -c config.json --catalog catalog.json | target-stitch -c persist.json > state.log; }
 
-tail -n1 state.log > state.json
+tail -n1 state.log > new-state.json
+
+if [ -s state.json ]
+then
+    aws s3 cp new-state.json "$TAP_STATE_S3_FILE_PATH"
+fi
